544e9ace0a7aedb21a1d219354ba7694546acdaa
Damon <nomadmonday@gmail.com>
2022-09-21 22:11:01 -0700

    Add transform method to soil processor.

--- ag-pipeline/scripts/process/soil_processor.py
+++ ag-pipeline/scripts/process/soil_processor.py
@@ -4,4 +4,14 @@ from base_csv_processor import BaseCSVProcessor
 
 class SoilProcessor(BaseCSVProcessor):
     def transform(self) -> None:
-        pass
+        self._df["hz_layer_weights"] = abs(self._df.hzdept - self._df.hzdepb) / self._df.hzdepb
+        self._df.om = self._df.om * (self._df.comppct / 100) * self._df.hz_layer_weights
+        self._df.cec = self._df.cec * (self._df.comppct / 100) * self._df.hz_layer_weights
+        self._df.ph = self._df.ph * (self._df.comppct / 100) * self._df.hz_layer_weights
+        self._df = self._df.groupby("mukey").agg({
+            "mukey_geometry": "max",
+            "om": "sum",
+            "cec": "sum",
+            "ph": "sum"
+        })
+        
